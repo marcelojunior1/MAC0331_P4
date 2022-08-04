5ecba12ee8e048868bc20ba89e6375398d0febe3
marcelojunior1 <marcelojunior@usp.br>
2022-08-04 10:16:56 -0300

    RC - Parte grafica - final

--- geocomp/projeto_4/ray_crossing.py
+++ geocomp/projeto_4/ray_crossing.py
@@ -14,15 +14,19 @@ def Ray_Crossing(l):
     ponto = l[0]
     del l[0]
 
-    print(ponto)
+    ponto.hilight("yellow")
 
     c = 0
     d = 0
 
+    control.sleep()
     for i in range(len(l)):
         segm_l = l[i]
-        p1 = segm_l.init
-        p2 = segm_l.to
+
+        segm_l.hilight()
+
+        p1 = Point(segm_l.init.x, segm_l.init.y)
+        p2 = Point(segm_l.to.x, segm_l.to.y)
 
         p1.x -= ponto.x
         p1.y -= ponto.y
@@ -31,6 +35,7 @@ def Ray_Crossing(l):
 
         if p1.x == 0 and p1.y == 0:
             print("Dentro")
+            ponto.hilight("green")
             return True
 
         teste_c = (p1.y > 0) is not (p2.y > 0)
@@ -42,15 +47,21 @@ def Ray_Crossing(l):
             if teste_d and intersecao_eixo_x(p1, p2) < 0:
                 d += 1
 
+        control.sleep()
+        segm_l.unhilight()
+
     if not ((c % 2 == 0 and d % 2 == 0) or (c % 2 != 0 and d % 2 != 0)):
         print("Dentro")
+        ponto.hilight("green")
         return True
 
     if c % 2 != 0:
         print("Dentro")
+        ponto.hilight("green")
         return True
     else:
         print("Fora")
+        ponto.hilight("red")
         return False
 
 
